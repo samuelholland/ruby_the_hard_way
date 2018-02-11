641bc02294ebe23a56240bf5787a68fccac8b0a9
Sam Holland <samuel.holland@gmail.com>
2018-02-10 20:24:16 -0500

    Add finished exercise 9

---
+++ ex9.rb
@@ -0,0 +1,19 @@
+# Here's some new strange stuff, remember to type it exactly.
+
+# String variable with days of the week in it
+days = "Mon Tue Wed Thu Fri Sat Sun"
+# String variable with Months of the year, each separated by a new line command
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+# Console log days of the week
+puts "Here are the days: #{days}"
+# Console log months of the year
+puts "Here are the months: #{months}"
+
+# Console log the text in the %q{} block exactly
+puts %q{
+There's something going on here.
+With this weird quote
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+}
