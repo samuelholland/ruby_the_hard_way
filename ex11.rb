e10d0d017ea99b1737c132cdc77a189a36717f97
Sam Holland <samuel.holland@gmail.com>
2018-02-10 20:53:18 -0500

    Added exercise 11

---
+++ ex11.rb
@@ -0,0 +1,21 @@
+print "How old are you? "
+age = gets.chomp
+print "How tall are you? "
+height = gets.chomp
+print "How much do you weigh? "
+weight = gets.chomp
+
+puts "So, you're #{age} old, #{height} tall, and #{weight} heavy."
+
+puts "hello.\n\n".chomp
+puts "hello.\n\n".chomp.chomp
+puts "hello.\n\n".chomp
+
+print "What is the first programming langauge you used? "
+language = gets.chomp
+print "And what did you use it with? "
+tools = gets.chomp
+print "And what did you think of it? "
+opinion = gets.chomp
+
+puts "So, you first coded in #{language} using #{tools} and you thought \"#{opinion}\" about it."
